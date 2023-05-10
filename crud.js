953ceded120b517a2ba7b2ce058ee6d0db2d8a28
modesto3103 <pedromesquita3103@gmail.com>
2023-05-09 22:43:08 -0300

    lcoal storage

--- crud.js
+++ crud.js
@@ -1,19 +1,50 @@
 document.querySelector("#salvar").addEventListener("click", cadastrar)
 
+let lista_tarefas = []
+
+window.addEventListener("load", () => {
+    lista_tarefas = JSON.parse(localStorage.getItem("lista_tarefas"))
+    lista_tarefas.forEach((tarefa) => {
+        document.querySelector("#tarefas").innerHTML += gerarCard(tarefa)
+
+    })
+})
+
 function cadastrar(){
+    const modal = bootstrap.Modal.getInstance(document.querySelector("#exampleModal"))
     let titulo = document.querySelector("#titulo").value
     let descricao = document.querySelector("#descricao").value
     let pontos = document.querySelector("#pontos").value
     let categoria = document.querySelector("#categoria").value
 
+    
     const tarefa = {
         titulo: titulo, 
         descricao: descricao,
         pontos: pontos,
         categoria: categoria,
     }
+    
+    if(tarefa.titulo.length == 0){
+        document.querySelector("#titulo").classList.add("is-invalid")
+        return
+
+    }
+
+    lista_tarefas.push(tarefa)
 
     document.querySelector("#tarefas").innerHTML += gerarCard(tarefa)
+
+    document.querySelector("#titulo").value = ""
+    document.querySelector("#descricao").value = ""
+
+    localStorage.setItem("lista_tarefas", JSON.stringify(lista_tarefas))
+
+    modal.hide()
+}
+
+function apagar(botao){
+    botao.parentNode.parentNode.remove()
 }
 
 function gerarCard(tarefa){
@@ -36,7 +67,7 @@ function gerarCard(tarefa){
                         <a href="#" class="btn btn-success">
                             <i class="bi bi-check-lg"></i>
                         </a>
-                        <a href="#" class="btn btn-danger">
+                        <a href="#" onClick='apagar(this)' class="btn btn-danger">
                             <i class="bi bi-trash"></i>
                         </a>
                     </div>
